710308e171aaf7abd613d3194f5bc770c4a4ef7b
archer <econarcher2010@gmail.com>
2023-03-24 11:07:44 +0800

    add react hook form

--- src/pages/WritePage1.jsx
+++ src/pages/WritePage1.jsx
@@ -8,6 +8,8 @@ import Button from "../components/Button";
 import axios from "axios";
 import Swal from "sweetalert2";
 
+import { useForm } from "react-hook-form";
+
 const url = "http://localhost:3000";
 const trial = {
   author: "al",
@@ -41,6 +43,8 @@ const trial = {
 
 
 const WritePage1 = () => {
+
+
   // why should i need to use async await, even in post?
   const addPost = async () => {
     try{
@@ -56,6 +60,12 @@ const WritePage1 = () => {
   };
 
 
+  const {register, handleSubmit, watch, formState:{errors}} = useForm();
+  const onSubmit = (data) => {console.log(typeof data);}
+
+  console.log(register);
+  // console.log(watch("example"));
+
   return (
     <>
       <Header />
@@ -64,7 +74,7 @@ const WritePage1 = () => {
       <button onClick={addPost}>send</button>
 
       <div className="flex justify-around py-4 pl-6 pr-6">
-        {/* start writing */}
+
         <div className="w-full rounded bg-white px-8 py-6 shadow-lg">
           <div className="max-w-3xl">
             <h1 className="my-4 text-2xl font-bold">發表評論</h1>
@@ -75,24 +85,27 @@ const WritePage1 = () => {
             <h1 className="my-4 text-2xl font-bold text-myFirstColor">
               換宿資訊
             </h1>
-            <form>
+
+            {/* FORM STARTS HERE */}
+            <form onSubmit={handleSubmit(onSubmit)}>
               {/* <!-- store name --> */}
               <div className="mb-6">
                 <label
                   htmlFor="storeName"
                   className="mb-2 block text-xl font-medium text-gray-900"
                 >
-                  換宿名稱
-                </label>
+                  換宿名稱  
                 <input
+                  // defaultValue="北山古洋樓"
                   type="text"
                   id="storeName"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
-                  placeholder="beishan"
-                  required
+                  placeholder="例如：北山古洋樓"
+                  {...register("storeName", {required: true})}
                 />
+                </label>
               </div>
-
+              {errors.storeNameRequired && <p>此欄位為必填</p>}
               {/* tel */}
               <div className="mb-6">
                 <label
@@ -105,9 +118,10 @@ const WritePage1 = () => {
                   type="text"
                   id="storePhone"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
-                  placeholder="07 123 4567"
-                  required
+                  placeholder="例如：071234567"
+                  {...register("storePhone", {required: true})}
                 />
+                {errors.storePhoneRequired && <p>此欄位為必填</p>}
               </div>
 
               {/* address*/}
@@ -118,31 +132,35 @@ const WritePage1 = () => {
                 >
                   換宿地址
                 </label>
+
                 <select
-                  defaultValue={"taipei"}
+                  defaultValue="taipei"
                   name=""
                   id="county"
                   className="mr-3 rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("county", {required: true})}
                 >
-                  <option value="taipei">台北市</option>
-                  <option value="kaohsiung">高雄市</option>
+                  <option value="台北市">台北市</option>
+                  <option value="高雄市">高雄市</option>
                 </select>
                 <select
-                  defaultValue={"daan"}
+                  defaultValue="大安區"
                   name=""
                   id="district"
                   className="rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("district", {required: true})}
                 >
-                  <option value="daan">大安區</option>
-                  <option value="nanzih">楠梓區</option>
+                  <option value="大安區">大安區</option>
+                  <option value="楠梓區">楠梓區</option>
                 </select>
                 <input
                   type="text"
                   id="road"
                   className="my-3 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
-                  placeholder="XX區XX路"
-                  required
+                  placeholder="XX路XX號"
+                  {...register("road", {required: true})}
                 />
+                {errors.roadRequired && <p>此欄位為必填</p>}
               </div>
 
               {/* work_span */}
@@ -154,18 +172,18 @@ const WritePage1 = () => {
                   換宿天數
                 </label>
                 <select
-                  defaultValue={"7days"}
+                  defaultValue="7"
                   name=""
                   id="workSpan"
                   className="rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("workSpan", {required: true})}
                 >
-                  <option value="7">1週</option>
-                  <option value="14">2週</option>
-                  <option value="21">3週</option>
-                  <option value="30">1個月</option>
+                  <option value="10" >1週－2週</option>
+                  <option value="25">3週－1個月</option>
                   <option value="60">大於1個月</option>
                 </select>
               </div>
+              {errors.workSpanRequired && <p>此欄位為必填</p>}
 
               {/* work_year */}
               <div className="mb-6">
@@ -176,10 +194,11 @@ const WritePage1 = () => {
                   換宿年份
                 </label>
                 <select
-                  defaultValue={"2023"}
+                  defaultValue="2022"
                   name=""
                   id="workYear"
                   className="mr-3 rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("workYear", {required: true})}
                 >
                   <option value="2019">2019</option>
                   <option value="2020">2020</option>
@@ -188,13 +207,14 @@ const WritePage1 = () => {
                   <option value="2023">2023</option>
                 </select>
                 <select
-                  defaultValue={"上半年"}
+                  // defaultValue={true} 
                   name=""
                   id="firstHalf"
                   className="mr-10 rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("firstHalf", {required: true})}
                 >
-                  <option value="true">上半年</option>
-                  <option value="false">下半年</option>
+                  <option value={true}>上半年</option>
+                  <option value={false}>下半年</option>
                 </select>
               </div>
 
@@ -208,12 +228,10 @@ const WritePage1 = () => {
                 </label>
                 <input
                   type="number"
-                  max="8"
-                  min="1"
                   id="workHour"
                   className="block w-36 rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
-                  placeholder="3"
-                  required
+                  placeholder="3小時"
+                  {...register("workHour", {min:1, max:8})}
                 />
               </div>
 
@@ -226,16 +244,17 @@ const WritePage1 = () => {
                   換宿類型
                 </label>
                 <select
-                  defaultValue={"normal"}
+                  defaultValue={false}
                   name=""
                   id="typePro"
                   className="rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 focus:border-myyFirstColorHover"
+                  {...register("typePro", {required: true})}
                 >
-                  <option value="normal">一般換宿</option>
-                  <option value="pro">專業換宿</option>
+                  <option value={false}>一般換宿</option>
+                  <option value={true}>專業換宿</option>
                 </select>
               </div>
-
+              <input type="submit" />
               <Button text="下一步"></Button>
             </form>
           </div>
