bc5a0b2cf64c7cf88c8cb366843c0c47b5f0af83
archer <econarcher2010@gmail.com>
2023-05-15 18:51:53 +0800

    update json server url

--- src/components/contexts/UserContext.jsx
+++ src/components/contexts/UserContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
-export const url = "json-server-vercel-two-brown.vercel.app";
+export const url = "https://demo-ynm1.onrender.com";
 
 // http://localhost:3000
 // https://demo-ynm1.onrender.com/users
