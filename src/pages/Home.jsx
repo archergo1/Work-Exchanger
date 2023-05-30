b87d38f47ff8fa1a0ac25bb9f20383844bfb1ffb
archer <econarcher2010@gmail.com>
2023-05-30 17:25:55 +0800

    add slogan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,15 @@ import OurLatest from "../components/OurLatest";
 export default function Home() {
   return (
     <div className="mx-auto max-w-screen-2xl bg-myFifthColor">
+      <div className="py-10">
+        <p className="mx-auto max-w-screen-xl rounded-lg bg-white px-8 py-4 text-3xl font-semibold text-myFirstColor leading-loose">
+          對生活、工作開始厭倦了嗎？
+          <br />
+          或許你可以給自己個機會，來趟打工換宿......
+          <br />
+          立刻看看過來人的心得分享，挑選喜歡的換宿吧！
+        </p>
+      </div>
       <BannerArea />
       <OurSelect />
       <OurLatest />
